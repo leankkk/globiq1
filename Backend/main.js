3e0ef1d973fb357336d5789a7914327f0d2feb37
Your Name <you@example.com>
2025-08-22 15:49:41 -0300

    Más funciones (7)

--- Backend/main.js
+++ Backend/main.js
@@ -1,43 +1,21 @@
 //Importando datos
-import fs from "fs";
+import fs, { Stats } from "fs";
 let data = JSON.parse(fs.readFileSync("factbook_limpio.json","utf-8"));
 
 //Declarando funciones útiles
 
-function limpiarpedido(string){
-let i;
-let c;
-let categoria = "";
-let dato = "";
-let valor = "";
-    for (i = 0; i < string.length; i++){
-    if (string[i] === "."){
-for (c = 0; c < i; c++){
-    if (categoria === ""){
-      categoria += string[c];
-    }
-    else if (dato === ""){
-        dato += string[c];
-    }
-    else if (valor === ""){
-        valor += string[c];
-    }
-}
-    }
-
-    }
-    let lista = [categoria,dato,valor]
-    return lista;
-}
-
-
 function contienedato(pais,dato) {
-
+    if (traerdato(pais,dato) != undefined){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 function traerdato(pais,dato) {
-dato = limpiarpedido(dato);
-return data[pais][dato[0]][dato[1]][dato[2]];
+dato = dato.split(".");
+return data[pais]?.[dato[0]]?.[dato[1]]?.[dato[2]];
 }
 
 function compararpaises(pais1,pais2,dato){
@@ -61,5 +39,4 @@ function paisdiario() {
 function datorandom(pais){
 
 }
-
-console.log(traerdato("india",limpiarpedido("people.population.total")));
+console.log(compararpaises("uruguay","argentina","people.population.total"));
