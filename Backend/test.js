04773da451c4bb5bd118bc3ba9f6165228979893
Your Name <you@example.com>
2025-09-25 15:28:33 -0300

    Cambios menores en listas y labels (24)

--- Backend/test.js
+++ Backend/test.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 //import { subscribeGETEvent, subscribePOSTEvent, startServer } from "soquetic";
 import { traer, contienedato, comparar, paisrandom, paisdiario, datorandom, cuentaexiste, crearcuenta, revisarlogin, actualizarstats, elegirpista } from "./Source/funciones.js";
-import { listadatos, listapaises, listadias, listalabels, listadatosB} from "./Source/listas.js";
+import { listadatos, listapaises, listadias, listalabels, listalabelsB, listadatosB} from "./Source/listas.js";
 let data = JSON.parse(fs.readFileSync("./Datos/factbook_clean.json","utf-8"));
 
 
