d7740a488f3a108537310560bc6c74ec0ab85690
Your Name <you@example.com>
2025-09-30 14:31:15 -0300

    nuevos cambios

--- Frontend/pantalla 5 (modo diario)/main.js
+++ Frontend/pantalla 5 (modo diario)/main.js
@@ -12,17 +12,16 @@ function guardarPistas(data){
 pista = data;
 }
 
-//Traer el país del día del back
 getEvent("obtenerPaisDiario",establecerPaisDiario);
 
-//Traer una pista al azar para el país del back. Opcional especificar categoría (todavia no se puede)
 postEvent("obtenerPista",{pais:paisdiario,categoria:categoria},guardarPistas)
 
 //
 
 if(input === paisdiario) {
-    alert('Adivinaste el país!')
+    alert('Adivinaste el país del día!')
 }
-else  {
-    //decirle a i/s que nos digan pista
+else {
+    let nuevoDiv = document.createElement('div')
+    div.textContent = 'Nueva Pista: ' + pista.label + pista.valor
 }
