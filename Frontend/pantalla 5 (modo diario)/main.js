8aa4ebb5c0aa0ce05675470497443a7c0ccbc029
Your Name <you@example.com>
2025-10-01 10:41:02 -0300

    cambios

--- Frontend/pantalla 5 (modo diario)/main.js
+++ Frontend/pantalla 5 (modo diario)/main.js
@@ -1,27 +1,33 @@
 connect2Server();
+let input = document.getElementById('input');
+let boton = document.getElementById('enviar');
 
-let input = document.getElementById('input')
+
 let paisdiario = "argentina";
 let categoria;
 let pista;
 
+
 function establecerPaisDiario(data){
-paisdiario = data;
-}
+    paisdiario = data;
+    }
 function guardarPistas(data){
 pista = data;
 }
+
+
+
 
 getEvent("obtenerPaisDiario",establecerPaisDiario);
 
 postEvent("obtenerPista",{pais:paisdiario,categoria:categoria},guardarPistas)
 
-//
 
-if(input === paisdiario) {
-    alert('Adivinaste el país del día!')
-}
-else {
-    let nuevoDiv = document.createElement('div')
-    div.textContent = 'Nueva Pista: ' + pista.label + pista.valor
-}
+boton.addEventListener('click', function () {
+    if (input.value.toLowerCase() === paisdiario.toLowerCase()) {
+        alert('¡Adivinaste el país!');
+    } 
+    else {
+        alert("Pista: " + (pista || "No hay pista disponible"));
+    }
+});
