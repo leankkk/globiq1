da87cd371943f1ae85641d6e6043ae6e1670685b
Your Name <you@example.com>
2025-10-03 12:08:59 -0300

    CAMBIOS HL Y MD. EN HL HAY Q TERMINAR DISEÑOA

--- Frontend/pantalla 4 (higher lower)/main.js
+++ Frontend/pantalla 4 (higher lower)/main.js
@@ -1,43 +1,31 @@
 
 connect2Server();
 
-
-let boton1 = document.getElementById('boton1');
-let boton2 = document.getElementById('boton2');
-let territorioActual = document.getElementById('territorioActual');
+const Valorizquierdo = 1000000;
+const Valorderecho = 1;
 
 
-let paisI = {
-    nombre: "ARGENTINA",
-    territorio: 12000000
-};
+const botonMayor = document.getElementById("btnMayor");
+const botonMenor = document.getElementById("btnMenor");
 
+function verificarRespuesta(opcion) {
+    const esMayor = Valorderecho > Valorizquierdo;
 
-let paisD = {
-    nombre: "PAÍS",
-    territorio: 0
-};
+    if (opcion === 'mayor' && esMayor === true) {
+        alert("¡Correcto!");
+    } else if (opcion === 'menor' && esMayor === false) {
+        alert("¡Correcto!");
+    } else {
+        alert("Incorrecto");
+    }
 
-let valores = [1000000, 2000000, 3000000, 4000000, 5000000];
-let indice = 0;
-
-
-boton1.addEventListener('click', function () {
-    let aumento = valores[indice];
-    paisD.territorio = paisI.territorio + aumento;
-    mostrarTerritorio();
-    avanzarIndice();
-});
-
-
-boton2.addEventListener('click', function () {
-    let disminuye = valores[indice];
-    paisD.territorio = paisI.territorio - disminucion;
-    mostrarTerritorio();
-    avanzarIndice();
-});
-
-
-function mostrarTerritorio() {
-    territorioActual.innerText = paisActual.territorio + " kiometroCuadrado";
+    alert("El país tenía: " + Valorderecho);
 }
+
+botonMayor.addEventListener("click", function() {
+    verificarRespuesta('mayor');
+});
+
+botonMenor.addEventListener("click", function() {
+    verificarRespuesta('menor');
+});
