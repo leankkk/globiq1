de8dbddd7695a095c0924f3855b9a3db4e713d26
Your Name <you@example.com>
2025-10-22 12:10:09 -0300

    CAMBIOS SNSJAHBSUJHKY

--- Frontend/pantalla 6 (login)/main.js
+++ Frontend/pantalla 6 (login)/main.js
@@ -1,29 +1,12 @@
-connect2Server();
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-let botonlogin = document.getElementById("loginBtn"); 
-let placeholderContraseña = document.getElementById("password");
-let placeholderUsuario = document.getElementById("usuario");
+signUpButton.addEventListener('click', () =>
+  container.classList.add('right-panel-active')
+);
 
-function togglePassword(id) { 
-    let input = document.getElementById(id);
-    input.type = input.type === "password" ? "text" : "password";
-}
 
-function realizarinicio(data) {
-    if (data.login) {
-        alert("Inicio de sesión completado.");
-        sessionStorage.setItem("usuario", placeholderUsuario.value);
-        window.location.href = "../home/index.html";
-    } else {
-        alert("Usuario o contraseña incorrecto.");
-    }
-}
-
-botonlogin.addEventListener("click", () => {
-    let infocuenta = {
-        nombre: placeholderUsuario.value,
-        contraseña: placeholderContraseña.value
-    };
-    console.log("Se activó el botón");
-    postEvent("iniciarSesion", infocuenta, realizarinicio);
-});
+signInButton.addEventListener('click', () =>
+  container.classList.remove('right-panel-active')
+);
