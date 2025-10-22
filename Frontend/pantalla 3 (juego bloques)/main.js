2924c973c4a6a7f3e511c55571379113d52f1cd8
Your Name <julianmarcozzi@gmail.com>
2025-10-22 12:11:59 -0300

    svg con pointer

--- Frontend/pantalla 3 (juego bloques)/main.js
+++ Frontend/pantalla 3 (juego bloques)/main.js
@@ -1,19 +1,2 @@
 connect2Server();
 
-
-let objetoAEnviar = {
-    input: {
-      comparacion: "mayor",
-      valor: 10,
-      categoria: "poblacion"
-    },
-    pais: "Argentina",
-    listaposibles: ["Brasil", "Chile"],
-    listadescartados: ["Uruguay"]
-  };
-  
-  
-  postEvent("evaluarPregunta", objetoAEnviar, (respuesta) => {
-    console.log("Respuesta del backend:", respuesta);
-  });
-  
