cb2be2354e23f5bf969da6e202301c5c4c7a6a8c
juliannmarcozzi <julianmarcozzi@gmail.com>
2025-10-25 00:04:44 -0300

    volvio el login og

--- Frontend/pantalla 6 (login)/main.js
+++ Frontend/pantalla 6 (login)/main.js
@@ -1,12 +1,26 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+connect2Server();
+let botonlogin = document.getElementById("loginBtn"); 
+let placeholderContraseña = document.getElementById("password");
+let placeholderUsuario = document.getElementById("usuario");
+function togglePassword(id) { 
+    let input = document.getElementById(id);
+    input.type = input.type === "password" ? "text" : "password";
+}
+function realizarinicio(data) {
+    if (data.login) {
+        alert("Inicio de sesión completado.");
+        sessionStorage.setItem("usuario", placeholderUsuario.value);
+        window.location.href = "../home/index.html";
+    } else {
+        alert("Usuario o contraseña incorrecto.");
+    }
+}
 
-signUpButton.addEventListener('click', () =>
-  container.classList.add('right-panel-active')
-);
-
-
-signInButton.addEventListener('click', () =>
-  container.classList.remove('right-panel-active')
-);
+botonlogin.addEventListener("click", () => {
+    let infocuenta = {
+        nombre: placeholderUsuario.value,
+        contraseña: placeholderContraseña.value
+    };
+    console.log("Se activó el botón");
+    postEvent("iniciarSesion", infocuenta, realizarinicio);
+});
