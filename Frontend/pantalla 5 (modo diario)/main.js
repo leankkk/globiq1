c80029a19d39e3443cf1e533fe70ba9ad2d3910f
Lcamde <luciocamde@gmail.com>
2025-10-29 10:46:56 -0300

    Estadísticas en Diario y Mayor o Menor (45)

--- Frontend/pantalla 5 (modo diario)/main.js
+++ Frontend/pantalla 5 (modo diario)/main.js
@@ -4,10 +4,30 @@ let boton = document.getElementById('enviar');
 let listaPistas = document.getElementById('listaPistas');
 let popup = document.getElementById('popup');
 let btnOk = document.getElementById('btn-ok');
-let intentosDOM = document.getElementById("cantidaddeintentosDOM")
+let intentosDOM = document.getElementById("cantidaddeintentosDOM");
+let usuario = sessionStorage.getItem("usuario") ?? "Sin usuario";
 
 let paisDiario;
 let intentos = 0;
+
+async function enviarstats(){
+  console.log("envio de stats iniciado")
+postEvent("recibirStats",{nombre:usuario},getStats);
+}
+
+function getStats(data){
+stats = data;
+console.log(stats);
+stats.stats.diario ??= {};
+let puntaje = stats.stats.diario.puntaje ?? Math.min(intentos, stats.stats.diario.puntaje);
+stats.stats.diario.puntaje ??= intentos;
+stats.stats.diario.puntaje = Math.min(intentos, stats.stats.diario.puntaje);
+console.log(puntaje, intentos, stats.stats.diario.puntaje);
+stats.stats.diario.puntaje ??= puntaje; 
+postEvent("guardarStats",{stats},guardarStats);
+}
+
+function guardarStats(){};
 
 function establecerPaisDiario(data) {
   paisDiario = data.label;
@@ -22,7 +42,7 @@ function mostrarPista(data) {
   listaPistas.scrollTop = listaPistas.scrollHeight; // hace que scrollee automáticamente
 }
 
-getEvent("obtenerPaisDiario", establecerPaisDiario);
+getEvent("obtenerPaisDiario",establecerPaisDiario)
 
 boton.addEventListener('click', () => {
   let respuesta = input.value.trim().toLowerCase();
@@ -30,6 +50,8 @@ boton.addEventListener('click', () => {
   if (respuesta === paisDiario.toLowerCase()) {
     intentosDOM.innerText = "Cantidad de pistas:  "+intentos;
     popup.style.display = "flex";
+    enviarstats();
+    input.disabled = "disabled";
   } else {
     // pedimos pista al backend
     postEvent("obtenerPista", {}, mostrarPista);
