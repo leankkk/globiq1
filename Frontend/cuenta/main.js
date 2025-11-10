28f3d420125b4700a14ff36bbf0dbb3579ec428c
Lcamde <luciocamde@gmail.com>
2025-11-10 10:34:31 -0300

    Sistema de stats en la pantalla de cuenta (61)

--- Frontend/cuenta/main.js
+++ Frontend/cuenta/main.js
@@ -9,3 +9,30 @@ nombreUsuario.textContent = usuario;
 let statsDiario = document.getElementById("statsDiario");
 let statsHL = document.getElementById("statsHL");
 let statsBloques = document.getElementById("statsBloques");
+let listaDiario = document.getElementById("listaDiario");
+let listaMayorMenor = document.getElementById("listaMayorMenor");
+let listaBloques = document.getElementById("listaBloques");
+let infoDiario;
+let infoMayorMenor;
+let infoBloques;
+let listaObjDiario;
+let listaObjMayorMenor;
+let listaObjBloques;
+
+
+//importar stats para mostrar en pantalla
+function mostrarStats(data){
+//guardando las variables
+infoDiario = data.stats.diario;
+infoMayorMenor = data.stats.mayormenor;
+infoBloques = data.stats.bloques;
+
+//armar los elementos de cada lista de stats
+for (let i = 0, listaObjDiario = Object.entries(infoDiario); i < listaObjDiario.length; i++) {
+let ("li"+i) = document.createElement("li")
+    listaDiario.appendChild()
+    
+}
+}
+
+postEvent("enviarStatsAlFront",{usuario: usuario}, mostrarStats);
